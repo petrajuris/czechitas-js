cef113edfac1ab7af2be4225c8c135d481e4fba3
Petra Juris <petra.gardavsk@gmail.com>
2021-10-19 14:59:32 +0200

    format file

--- ukol.js
+++ ukol.js
@@ -1,4 +1,4 @@
-let indian
+let indian;
 let radek = "";
 for (indian = 1; indian < 10; indian++) {
   if (indian % 3) {
@@ -9,15 +9,15 @@ for (indian = 1; indian < 10; indian++) {
     radek = "";
   }
 }
-console.log(indian + " little Indian boys.\n")
+console.log(indian + " little Indian boys.\n");
 
 for (indian = 10; indian > 1; indian--) {
-    if (indian != 8 && indian != 5 && indian != 2) {
-      radek = radek + indian + " " + "little,";
-    } else {
-      radek = radek + indian + " " + "little Indians";
-      console.log(radek);
-      radek = "";
-    }
+  if (indian != 8 && indian != 5 && indian != 2) {
+    radek = radek + indian + " " + "little,";
+  } else {
+    radek = radek + indian + " " + "little Indians";
+    console.log(radek);
+    radek = "";
   }
-  console.log(indian + " little Indian boy.")
+}
+console.log(indian + " little Indian boy.");
